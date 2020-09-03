4ec8d76890caf7089995f1f5f88597f9e27d479a
chrisnabayuaji <chrisnabayuaji@gmail.com>
2020-09-03 20:25:22 +0700

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace LabelApp
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.btnRefresh = new System.Windows.Forms.Button();
+      this.btnSearch = new System.Windows.Forms.Button();
+      this.txtSearch = new System.Windows.Forms.TextBox();
+      this.dgItem = new System.Windows.Forms.DataGridView();
+      this.btnNew = new System.Windows.Forms.Button();
+      this.gbForm = new System.Windows.Forms.GroupBox();
+      this.txtItemIdOld = new System.Windows.Forms.TextBox();
+      this.btnDelete = new System.Windows.Forms.Button();
+      this.btnCancel = new System.Windows.Forms.Button();
+      this.btnSave = new System.Windows.Forms.Button();
+      this.txtItemChinese = new System.Windows.Forms.TextBox();
+      this.lblItemChinese = new System.Windows.Forms.Label();
+      this.txtItemName3 = new System.Windows.Forms.TextBox();
+      this.lblItemName3 = new System.Windows.Forms.Label();
+      this.txtItemName2 = new System.Windows.Forms.TextBox();
+      this.lblItemName2 = new System.Windows.Forms.Label();
+      this.txtItemName1 = new System.Windows.Forms.TextBox();
+      this.lblItemName1 = new System.Windows.Forms.Label();
+      this.txtItemId = new System.Windows.Forms.TextBox();
+      this.lblItemId = new System.Windows.Forms.Label();
+      this.groupBox1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.dgItem)).BeginInit();
+      this.gbForm.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.btnRefresh);
+      this.groupBox1.Controls.Add(this.btnSearch);
+      this.groupBox1.Controls.Add(this.txtSearch);
+      this.groupBox1.Controls.Add(this.dgItem);
+      this.groupBox1.Controls.Add(this.btnNew);
+      this.groupBox1.Location = new System.Drawing.Point(2, 1);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(596, 407);
+      this.groupBox1.TabIndex = 5;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "Data Barang";
+      // 
+      // btnRefresh
+      // 
+      this.btnRefresh.Location = new System.Drawing.Point(519, 19);
+      this.btnRefresh.Name = "btnRefresh";
+      this.btnRefresh.Size = new System.Drawing.Size(70, 23);
+      this.btnRefresh.TabIndex = 9;
+      this.btnRefresh.Text = "REFRESH";
+      this.btnRefresh.UseVisualStyleBackColor = true;
+      this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+      // 
+      // btnSearch
+      // 
+      this.btnSearch.Location = new System.Drawing.Point(463, 19);
+      this.btnSearch.Name = "btnSearch";
+      this.btnSearch.Size = new System.Drawing.Size(50, 23);
+      this.btnSearch.TabIndex = 8;
+      this.btnSearch.Text = "CARI";
+      this.btnSearch.UseVisualStyleBackColor = true;
+      this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+      // 
+      // txtSearch
+      // 
+      this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.txtSearch.Location = new System.Drawing.Point(295, 20);
+      this.txtSearch.Name = "txtSearch";
+      this.txtSearch.Size = new System.Drawing.Size(162, 22);
+      this.txtSearch.TabIndex = 7;
+      // 
+      // dgItem
+      // 
+      this.dgItem.AllowUserToAddRows = false;
+      this.dgItem.AllowUserToDeleteRows = false;
+      this.dgItem.AllowUserToResizeColumns = false;
+      this.dgItem.AllowUserToResizeRows = false;
+      this.dgItem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dgItem.Location = new System.Drawing.Point(7, 48);
+      this.dgItem.MultiSelect = false;
+      this.dgItem.Name = "dgItem";
+      this.dgItem.ReadOnly = true;
+      this.dgItem.RowHeadersVisible = false;
+      this.dgItem.Size = new System.Drawing.Size(582, 351);
+      this.dgItem.TabIndex = 6;
+      this.dgItem.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgItem_CellClick);
+      this.dgItem.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgItem_CellDoubleClick);
+      // 
+      // btnNew
+      // 
+      this.btnNew.Location = new System.Drawing.Point(7, 19);
+      this.btnNew.Name = "btnNew";
+      this.btnNew.Size = new System.Drawing.Size(107, 23);
+      this.btnNew.TabIndex = 5;
+      this.btnNew.Text = "TAMBAH BARU";
+      this.btnNew.UseVisualStyleBackColor = true;
+      this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+      // 
+      // gbForm
+      // 
+      this.gbForm.Controls.Add(this.txtItemIdOld);
+      this.gbForm.Controls.Add(this.btnDelete);
+      this.gbForm.Controls.Add(this.btnCancel);
+      this.gbForm.Controls.Add(this.btnSave);
+      this.gbForm.Controls.Add(this.txtItemChinese);
+      this.gbForm.Controls.Add(this.lblItemChinese);
+      this.gbForm.Controls.Add(this.txtItemName3);
+      this.gbForm.Controls.Add(this.lblItemName3);
+      this.gbForm.Controls.Add(this.txtItemName2);
+      this.gbForm.Controls.Add(this.lblItemName2);
+      this.gbForm.Controls.Add(this.txtItemName1);
+      this.gbForm.Controls.Add(this.lblItemName1);
+      this.gbForm.Controls.Add(this.txtItemId);
+      this.gbForm.Controls.Add(this.lblItemId);
+      this.gbForm.Location = new System.Drawing.Point(604, 1);
+      this.gbForm.Name = "gbForm";
+      this.gbForm.Size = new System.Drawing.Size(402, 184);
+      this.gbForm.TabIndex = 6;
+      this.gbForm.TabStop = false;
+      this.gbForm.Text = "Form Barang";
+      // 
+      // txtItemIdOld
+      // 
+      this.txtItemIdOld.Enabled = false;
+      this.txtItemIdOld.Location = new System.Drawing.Point(29, 152);
+      this.txtItemIdOld.Name = "txtItemIdOld";
+      this.txtItemIdOld.Size = new System.Drawing.Size(66, 20);
+      this.txtItemIdOld.TabIndex = 13;
+      this.txtItemIdOld.Visible = false;
+      // 
+      // btnDelete
+      // 
+      this.btnDelete.Enabled = false;
+      this.btnDelete.Location = new System.Drawing.Point(318, 152);
+      this.btnDelete.Name = "btnDelete";
+      this.btnDelete.Size = new System.Drawing.Size(75, 23);
+      this.btnDelete.TabIndex = 12;
+      this.btnDelete.Text = "HAPUS";
+      this.btnDelete.UseVisualStyleBackColor = true;
+      this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+      // 
+      // btnCancel
+      // 
+      this.btnCancel.Enabled = false;
+      this.btnCancel.Location = new System.Drawing.Point(182, 152);
+      this.btnCancel.Name = "btnCancel";
+      this.btnCancel.Size = new System.Drawing.Size(75, 23);
+      this.btnCancel.TabIndex = 11;
+      this.btnCancel.Text = "BATAL";
+      this.btnCancel.UseVisualStyleBackColor = true;
+      this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+      // 
+      // btnSave
+      // 
+      this.btnSave.Enabled = false;
+      this.btnSave.Location = new System.Drawing.Point(101, 152);
+      this.btnSave.Name = "btnSave";
+      this.btnSave.Size = new System.Drawing.Size(75, 23);
+      this.btnSave.TabIndex = 10;
+      this.btnSave.Text = "SIMPAN";
+      this.btnSave.UseVisualStyleBackColor = true;
+      this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+      // 
+      // txtItemChinese
+      // 
+      this.txtItemChinese.Enabled = false;
+      this.txtItemChinese.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+      this.txtItemChinese.Location = new System.Drawing.Point(101, 126);
+      this.txtItemChinese.Name = "txtItemChinese";
+      this.txtItemChinese.Size = new System.Drawing.Size(292, 20);
+      this.txtItemChinese.TabIndex = 9;
+      // 
+      // lblItemChinese
+      // 
+      this.lblItemChinese.AutoSize = true;
+      this.lblItemChinese.Location = new System.Drawing.Point(32, 129);
+      this.lblItemChinese.Name = "lblItemChinese";
+      this.lblItemChinese.Size = new System.Drawing.Size(63, 13);
+      this.lblItemChinese.TabIndex = 8;
+      this.lblItemChinese.Text = "Huruf China";
+      // 
+      // txtItemName3
+      // 
+      this.txtItemName3.Enabled = false;
+      this.txtItemName3.Location = new System.Drawing.Point(101, 100);
+      this.txtItemName3.Name = "txtItemName3";
+      this.txtItemName3.Size = new System.Drawing.Size(292, 20);
+      this.txtItemName3.TabIndex = 7;
+      // 
+      // lblItemName3
+      // 
+      this.lblItemName3.AutoSize = true;
+      this.lblItemName3.Location = new System.Drawing.Point(14, 103);
+      this.lblItemName3.Name = "lblItemName3";
+      this.lblItemName3.Size = new System.Drawing.Size(81, 13);
+      this.lblItemName3.TabIndex = 6;
+      this.lblItemName3.Text = "Nama Barang 3";
+      // 
+      // txtItemName2
+      // 
+      this.txtItemName2.Enabled = false;
+      this.txtItemName2.Location = new System.Drawing.Point(101, 74);
+      this.txtItemName2.Name = "txtItemName2";
+      this.txtItemName2.Size = new System.Drawing.Size(292, 20);
+      this.txtItemName2.TabIndex = 5;
+      // 
+      // lblItemName2
+      // 
+      this.lblItemName2.AutoSize = true;
+      this.lblItemName2.Location = new System.Drawing.Point(14, 77);
+      this.lblItemName2.Name = "lblItemName2";
+      this.lblItemName2.Size = new System.Drawing.Size(81, 13);
+      this.lblItemName2.TabIndex = 4;
+      this.lblItemName2.Text = "Nama Barang 2";
+      // 
+      // txtItemName1
+      // 
+      this.txtItemName1.Enabled = false;
+      this.txtItemName1.Location = new System.Drawing.Point(101, 48);
+      this.txtItemName1.Name = "txtItemName1";
+      this.txtItemName1.Size = new System.Drawing.Size(292, 20);
+      this.txtItemName1.TabIndex = 3;
+      // 
+      // lblItemName1
+      // 
+      this.lblItemName1.AutoSize = true;
+      this.lblItemName1.Location = new System.Drawing.Point(14, 51);
+      this.lblItemName1.Name = "lblItemName1";
+      this.lblItemName1.Size = new System.Drawing.Size(81, 13);
+      this.lblItemName1.TabIndex = 2;
+      this.lblItemName1.Text = "Nama Barang 1";
+      // 
+      // txtItemId
+      // 
+      this.txtItemId.Enabled = false;
+      this.txtItemId.Location = new System.Drawing.Point(101, 21);
+      this.txtItemId.Name = "txtItemId";
+      this.txtItemId.Size = new System.Drawing.Size(292, 20);
+      this.txtItemId.TabIndex = 1;
+      // 
+      // lblItemId
+      // 
+      this.lblItemId.AutoSize = true;
+      this.lblItemId.Location = new System.Drawing.Point(26, 25);
+      this.lblItemId.Name = "lblItemId";
+      this.lblItemId.Size = new System.Drawing.Size(69, 13);
+      this.lblItemId.TabIndex = 0;
+      this.lblItemId.Text = "Kode Barang";
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1009, 412);
+      this.Controls.Add(this.gbForm);
+      this.Controls.Add(this.groupBox1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "MainForm";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "APLIKASI LABEL BARANG";
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.dgItem)).EndInit();
+      this.gbForm.ResumeLayout(false);
+      this.gbForm.PerformLayout();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.Button btnRefresh;
+    private System.Windows.Forms.Button btnSearch;
+    private System.Windows.Forms.TextBox txtSearch;
+    private System.Windows.Forms.DataGridView dgItem;
+    private System.Windows.Forms.Button btnNew;
+    private System.Windows.Forms.GroupBox gbForm;
+    private System.Windows.Forms.TextBox txtItemChinese;
+    private System.Windows.Forms.Label lblItemChinese;
+    private System.Windows.Forms.TextBox txtItemName3;
+    private System.Windows.Forms.Label lblItemName3;
+    private System.Windows.Forms.TextBox txtItemName2;
+    private System.Windows.Forms.Label lblItemName2;
+    private System.Windows.Forms.TextBox txtItemName1;
+    private System.Windows.Forms.Label lblItemName1;
+    private System.Windows.Forms.TextBox txtItemId;
+    private System.Windows.Forms.Label lblItemId;
+    private System.Windows.Forms.Button btnDelete;
+    private System.Windows.Forms.Button btnCancel;
+    private System.Windows.Forms.Button btnSave;
+    private System.Windows.Forms.TextBox txtItemIdOld;
+  }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace LabelApp
+{
+  public partial class MainForm : Form
+  {
+    OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=labelbarang.accdb");
+
+    public MainForm()
+    {
+      InitializeComponent();
+      initialize_dg();
+      show_data();
+    }
+
+    private void initialize_dg() {
+      DataGridViewColumn itemId = new DataGridViewTextBoxColumn();
+      itemId.DataPropertyName = "itemId";
+      itemId.HeaderText = "Kode Barang";
+      itemId.Name = "itemId";
+      itemId.Width = 100;
+      dgItem.Columns.Add(itemId);
+
+      DataGridViewColumn itemName1 = new DataGridViewTextBoxColumn();
+      itemName1.DataPropertyName = "itemName1";
+      itemName1.HeaderText = "Nama Barang 1";
+      itemName1.Name = "itemName1";
+      itemName1.Width = 120;
+      dgItem.Columns.Add(itemName1);
+
+      DataGridViewColumn itemName2 = new DataGridViewTextBoxColumn();
+      itemName2.DataPropertyName = "itemName2";
+      itemName2.HeaderText = "Nama Barang 2";
+      itemName2.Name = "itemName2";
+      itemName2.Width = 120;
+      dgItem.Columns.Add(itemName2);
+
+      DataGridViewColumn itemName3 = new DataGridViewTextBoxColumn();
+      itemName3.DataPropertyName = "itemName3";
+      itemName3.HeaderText = "Nama Barang 3";
+      itemName3.Name = "itemName3";
+      itemName3.Width = 120;
+      dgItem.Columns.Add(itemName3);
+
+      DataGridViewColumn itemChinese = new DataGridViewTextBoxColumn();
+      itemChinese.DataPropertyName = "itemChinese";
+      itemChinese.HeaderText = "Huruf China";
+      itemChinese.Name = "itemChinese";
+      itemChinese.Width = 100;
+      dgItem.Columns.Add(itemChinese);
+    }
+
+    private void show_data() {
+      String search = txtSearch.Text;
+      search = search.Replace("'", "''");
+      dgItem.Rows.Clear();
+
+      try
+      {
+        conn.Open();
+        String query = "SELECT * FROM items WHERE item_name_1 LIKE '%"+search+ "%' OR item_name_2 LIKE '%" + search + "%' OR item_name_3 LIKE '%" + search + "%'";
+        OleDbCommand cmd = new OleDbCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = query;
+        cmd.Connection = conn;
+
+        OleDbDataReader reader = cmd.ExecuteReader();
+        if (reader.HasRows)
+        {
+          while (reader.Read())
+          {
+            int i = dgItem.Rows.Add();
+            dgItem.Rows[i].Cells[0].Value = reader["item_id"].ToString();
+            dgItem.Rows[i].Cells[1].Value = reader["item_name_1"].ToString();
+            dgItem.Rows[i].Cells[2].Value = reader["item_name_2"].ToString();
+            dgItem.Rows[i].Cells[3].Value = reader["item_name_3"].ToString();
+            dgItem.Rows[i].Cells[4].Value = reader["item_chinese"].ToString();
+          }
+        }
+        reader.Close();
+        conn.Close();
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.ToString(), "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+      }
+    }
+
+    private void save_item()
+    {
+      String itemId = txtItemId.Text.Replace("'", "''");
+      String itemIdOld = txtItemIdOld.Text.Replace("'", "''");
+      String itemName1 = txtItemName1.Text.Replace("'", "''");
+      String itemName2 = txtItemName2.Text.Replace("'", "''");
+      String itemName3 = txtItemName3.Text.Replace("'", "''");
+      String itemChinese = txtItemChinese.Text.Replace("'", "''");
+
+      //validasi form
+      if (itemId == "" || itemName1 == "" || itemName2 == "" || itemName3 == "" || itemChinese == "") {
+        MessageBox.Show("Semua kolom tidak boleh kosong", "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+      }
+      else
+      {
+        if (itemIdOld == "")
+        {
+          //insert new item
+          if (check_id(itemId))
+          {
+            MessageBox.Show("Kode Barang sudah ada di sistem!", "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+          }
+          else
+          {
+            try
+            {
+              conn.Open();
+              String query = "INSERT INTO items (item_id, item_name_1, item_name_2, item_name_3, item_chinese) VALUES (@itemId, @itemName1, @itemName2, @itemName3, itemChinese)";
+              OleDbCommand cmd = new OleDbCommand();
+              cmd.CommandType = CommandType.Text;
+              cmd.CommandText = query;
+              cmd.Parameters.AddWithValue("@itemId", itemId);
+              cmd.Parameters.AddWithValue("@itemName1", itemName1);
+              cmd.Parameters.AddWithValue("@itemName2", itemName2);
+              cmd.Parameters.AddWithValue("@itemName3", itemName3);
+              cmd.Parameters.AddWithValue("@itemChinese", itemChinese);
+              cmd.Connection = conn;
+
+              cmd.ExecuteNonQuery();
+              conn.Close();
+              MessageBox.Show("Data berhasil disimpan", "Berhasil", MessageBoxButtons.OK, MessageBoxIcon.Information);
+              reset_item();
+              show_data();
+            }
+            catch (Exception ex)
+            {
+              MessageBox.Show(ex.ToString(), "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+          }
+        }
+        else
+        {
+          //update item
+          if (txtItemId.Text == txtItemIdOld.Text) {
+            update_data();
+          }
+          else
+          {
+            if (check_id(itemId))
+            {
+              MessageBox.Show("Kode Barang sudah ada di sistem!", "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+              update_data();
+            }
+          }
+        }
+      }
+    }
+
+    private void update_data() {
+      String itemId = txtItemId.Text.Replace("'", "''"); ;
+      String itemIdOld = txtItemIdOld.Text.Replace("'", "''"); ;
+      String itemName1 = txtItemName1.Text.Replace("'", "''"); ;
+      String itemName2 = txtItemName2.Text.Replace("'", "''"); ;
+      String itemName3 = txtItemName3.Text.Replace("'", "''"); ;
+      String itemChinese = txtItemChinese.Text.Replace("'", "''"); ;
+
+      try
+      {
+        conn.Open();
+        String query = "UPDATE items SET item_id = @itemId, item_name_1 = @itemName1, item_name_2 = @itemName2, item_name_3 = @itemName3, item_chinese = @itemChinese WHERE item_id = @itemIdOld";
+        OleDbCommand cmd = new OleDbCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = query;
+        cmd.Parameters.AddWithValue("@itemId", itemId);
+        cmd.Parameters.AddWithValue("@itemName1", itemName1);
+        cmd.Parameters.AddWithValue("@itemName2", itemName2);
+        cmd.Parameters.AddWithValue("@itemName3", itemName3);
+        cmd.Parameters.AddWithValue("@itemChinese", itemChinese);
+        cmd.Parameters.AddWithValue("@itemIdOld", itemIdOld);
+        cmd.Connection = conn;
+
+        cmd.ExecuteNonQuery();
+        conn.Close();
+        MessageBox.Show("Data berhasil diubah", "Berhasil", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        reset_item();
+        show_data();
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.ToString(), "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+      }
+    }
+
+    private Boolean check_id(String itemId) {
+      try
+      {
+        conn.Open();
+        String query = @"SELECT * FROM items WHERE item_id = @param1";
+        OleDbCommand cmd = new OleDbCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = query;
+        cmd.Parameters.AddWithValue("@param1", itemId);
+        cmd.Connection = conn;
+
+        OleDbDataReader reader = cmd.ExecuteReader();
+        if (reader.HasRows)
+        {
+          reader.Close();
+          conn.Close();
+          return true;
+        }
+        else
+        {
+          reader.Close();
+          conn.Close();
+          return false;
+        }
+      }
+      catch(Exception ex) {
+        MessageBox.Show(ex.ToString(), "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        return false;
+      }
+    }
+
+    private void new_item()
+    {
+      txtItemIdOld.Enabled = true;
+      txtItemIdOld.Text = "";
+      txtItemId.Enabled = true;
+      txtItemId.Text = "";
+      txtItemName1.Enabled = true;
+      txtItemName1.Text = "";
+      txtItemName2.Enabled = true;
+      txtItemName2.Text = "";
+      txtItemName3.Enabled = true;
+      txtItemName3.Text = "";
+      txtItemChinese.Enabled = true;
+      txtItemChinese.Text = "";
+
+      btnSave.Enabled = true;
+      btnCancel.Enabled = true;
+      btnDelete.Enabled = false;
+    }
+
+    private void reset_item()
+    {
+      txtItemIdOld.Enabled = false;
+      txtItemIdOld.Text = "";
+      txtItemId.Enabled = false;
+      txtItemId.Text = "";
+      txtItemName1.Enabled = false;
+      txtItemName1.Text = "";
+      txtItemName2.Enabled = false;
+      txtItemName2.Text = "";
+      txtItemName3.Enabled = false;
+      txtItemName3.Text = "";
+      txtItemChinese.Enabled = false;
+      txtItemChinese.Text = "";
+
+      btnSave.Enabled = false;
+      btnCancel.Enabled = false;
+      btnDelete.Enabled = false;      
+    }
+
+    private void btnNew_Click(object sender, EventArgs e)
+    {
+      new_item();
+    }
+
+    private void btnCancel_Click(object sender, EventArgs e)
+    {
+      reset_item();
+    }
+
+    private void btnSave_Click(object sender, EventArgs e)
+    {
+      save_item();
+    }
+
+    private void btnSearch_Click(object sender, EventArgs e)
+    {
+      show_data();
+    }
+
+    private void btnRefresh_Click(object sender, EventArgs e)
+    {
+      txtSearch.Text = "";
+      show_data();
+    }
+
+    private void dgItem_CellClick(object sender, DataGridViewCellEventArgs e)
+    {
+      int i = e.RowIndex;
+      dgItem.Rows[i].Selected = true;
+    }
+
+    private void dgItem_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+    {
+      int i = e.RowIndex;
+      dgItem.Rows[i].Selected = true;
+      String itemId = dgItem.Rows[i].Cells[0].Value.ToString();
+      edit_data(itemId);
+    }
+
+    private void edit_data(String itemId) {
+      try
+      {
+        conn.Open();
+        String query = @"SELECT * FROM items WHERE item_id = @param1";
+        OleDbCommand cmd = new OleDbCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = query;
+        cmd.Parameters.AddWithValue("@param1", itemId);
+        cmd.Connection = conn;
+
+        OleDbDataReader reader = cmd.ExecuteReader();
+        if (reader.HasRows)
+        {
+          while (reader.Read()) {
+            txtItemId.Enabled = true;
+            txtItemId.Text = reader["item_id"].ToString();
+            txtItemIdOld.Enabled = true;
+            txtItemIdOld.Text = reader["item_id"].ToString();
+            txtItemName1.Enabled = true;
+            txtItemName1.Text = reader["item_name_1"].ToString();
+            txtItemName2.Enabled = true;
+            txtItemName2.Text = reader["item_name_2"].ToString();
+            txtItemName3.Enabled = true;
+            txtItemName3.Text = reader["item_name_3"].ToString();
+            txtItemChinese.Enabled = true;
+            txtItemChinese.Text = reader["item_chinese"].ToString();
+
+            btnSave.Enabled = true;
+            btnCancel.Enabled = true;
+            btnDelete.Enabled = true;
+          }
+        }
+        reader.Close();
+        conn.Close();
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.ToString(), "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+      }
+    }
+
+    private void btnDelete_Click(object sender, EventArgs e)
+    {
+      DialogResult result = MessageBox.Show("Apakah Anda yakin menghapus data ini?\nAksi ini tidak bisa dikembalikan!", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+      if(result == DialogResult.Yes)
+      {
+        delete_data();
+      }
+    }
+
+    private void delete_data() {
+      try
+      {
+        conn.Open();
+        String query = "DELETE FROM items WHERE item_id = @itemOldId";
+        OleDbCommand cmd = new OleDbCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = query;
+        cmd.Parameters.AddWithValue("@itemId", txtItemIdOld.Text);
+        cmd.Connection = conn;
+
+        cmd.ExecuteNonQuery();
+        conn.Close();
+        MessageBox.Show("Data berhasil dihapus", "Berhasil", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        reset_item();
+        show_data();
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.ToString(), "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+      }
+    }
+  }
+}
